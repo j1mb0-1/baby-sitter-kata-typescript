4cb9e0f07ff9491d578cbcd761949f8dbd3a2692
jamesbaker <jabaker1927@gmail.com>
2022-03-27 14:39:04 -0400

    Support recording times in timesheet

--- src/domain/babySittingTimeSheet.spec.ts
+++ src/domain/babySittingTimeSheet.spec.ts
@@ -20,4 +20,105 @@ describe("baby sitting time sheet", () => {
     expect(timeSheet.endedTime).toBeUndefined();
     expect(timeSheet.bedTime).toBeUndefined();
   });
+
+  it("should set started time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const startedTime: Date = new Date("2022-03-27T21:00:00.000Z");
+    timeSheet.startedTime = startedTime;
+
+    expect(timeSheet.startedTime).toEqual(startedTime);
+  });
+
+  it("should not set started time when it is before it's jobs start time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const startedTime: Date = new Date("2022-03-27T17:00:00.000Z");
+
+    expect(() => {
+      timeSheet.startedTime = startedTime;
+    }).toThrowError();
+  });
+
+  it("should not set started time when it is after it's jobs end time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const startedTime: Date = new Date("2022-03-29T17:00:00.000Z");
+
+    expect(() => {
+      timeSheet.startedTime = startedTime;
+    }).toThrowError();
+  });
+
+  it("should set ended time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const endedTime: Date = new Date("2022-03-28T08:00:00.000Z");
+    timeSheet.endedTime = endedTime;
+
+    expect(timeSheet.endedTime).toEqual(endedTime);
+  });
+
+  it("should not set ended time when it is after it's jobs end time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const endedTime: Date = new Date("2022-03-29T17:00:00.000Z");
+
+    expect(() => {
+      timeSheet.endedTime = endedTime;
+    }).toThrowError();
+  });
+
+  it("should not set ended time when it is before it's jobs start time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const endedTime: Date = new Date("2022-03-27T20:00:00.000Z");
+
+    expect(() => {
+      timeSheet.endedTime = endedTime;
+    }).toThrowError();
+  });
+
+  it("should set bed time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const bedTime: Date = new Date("2022-03-27T23:00:00.000Z");
+    timeSheet.bedTime = bedTime;
+
+    expect(timeSheet.bedTime).toEqual(bedTime);
+  });
+
+  it("should not set a bed time when it is before it's jobs start time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const bedTime: Date = new Date("2022-03-27T20:00:00.000Z");
+
+    expect(() => {
+      timeSheet.bedTime = bedTime;
+    }).toThrowError();
+  });
+
+  it("should not set a bed time when it is after it's jobs end time", () => {
+    const timeSheet: BabySittingTimeSheet = createValidatedTimeSheet();
+
+    const bedTime: Date = new Date("2022-03-28T20:00:00.000Z");
+
+    expect(() => {
+      timeSheet.bedTime = bedTime;
+    }).toThrowError();
+  });
+
+  const createValidatedTimeSheet = (): BabySittingTimeSheet => {
+    const startTime: Date = new Date("2022-03-27T21:00:00.000Z");
+    const endTime: Date = new Date("2022-03-28T08:00:00.000Z");
+    const job: BabySittingJob = new BabySittingJob(
+      startTime,
+      endTime,
+      12,
+      8,
+      16
+    );
+    const timeSheet: BabySittingTimeSheet = new BabySittingTimeSheet(job);
+    return timeSheet;
+  };
 });
--- src/domain/babySittingTimeSheet.ts
+++ src/domain/babySittingTimeSheet.ts
@@ -18,11 +18,47 @@ export class BabySittingTimeSheet {
     return this._startedTime;
   }
 
+  set startedTime(value: Date | undefined) {
+    if (value && value < this._job.startTime) {
+      throw Error(
+        "Cannot record a started time that is before a job's start time"
+      );
+    }
+    if (value && value > this._job.endTime) {
+      throw Error(
+        "Cannot record a started time that is after a job's end time"
+      );
+    }
+    this._startedTime = value;
+  }
+
   get endedTime(): Date | undefined {
     return this._endedTime;
   }
 
+  set endedTime(value: Date | undefined) {
+    if (value && value < this._job.startTime) {
+      throw Error(
+        "Cannot record an ended time that is before a job's start time"
+      );
+    }
+    if (value && value > this._job.endTime) {
+      throw Error("Cannot record an ended time that is after a job's end time");
+    }
+    this._endedTime = value;
+  }
+
   get bedTime(): Date | undefined {
     return this._bedTime;
+  }
+
+  set bedTime(value: Date | undefined) {
+    if (value && value < this._job.startTime) {
+      throw Error("Cannot record a bed time that is before a job's start time");
+    }
+    if (value && value > this._job.endTime) {
+      throw Error("Cannot record an bed time that is after a job's end time");
+    }
+    this._bedTime = value;
   }
 }
